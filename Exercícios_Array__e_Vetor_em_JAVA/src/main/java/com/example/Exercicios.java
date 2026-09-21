1019675459ce301ec68f351013be57b77d3916f0
Leo300609 <leoalmeida.canto@gmail.com>
2026-09-21 16:31:15 -0300

    Refactor age input handling to allow dynamic array size and calculate sum and average

--- Exercícios_Array__e_Vetor_em_JAVA/src/main/java/com/example/Exercicios.java
+++ Exercícios_Array__e_Vetor_em_JAVA/src/main/java/com/example/Exercicios.java
@@ -4,17 +4,30 @@ import java.util.Scanner;
 public class Exercicios {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numeros = new int[5];
+
+        System.out.println("Informe quantas idades você deseja apresentar");
+        int tamanho = scanner.nextInt();
+
+        int[] numeros = new int[tamanho];
+        int soma = 0;
+        double media = 0;
         
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < tamanho; i++){
             System.out.println("Diga a idade do " + (i + 1) +"° aluno:");
             numeros[i] = scanner.nextInt();
+            soma += numeros[i];
         }
+
+        media = soma / tamanho;
         
         for(int i = 0; i < 5; i++){
-            System.out.println("Idade do " + (i + 1) + "° aluno: " + numeros[i]);
+            System.out.println("Idade do " + (i + 1) + "° aluno: " + numeros[i] + "\n \n");
+            System.out.println("Soma das do idades: " + soma + "\n \n");
+            System.out.println("Média das idades:" + numeros[i] + "\n \n");
+
         }
         
         scanner.close();
     }
 }
+
